73c135e15d27aa5c0033f9cee2df1722138f7f9d
Woody White <woody.white276@gmail.com>
2022-10-05 13:56:18 -0400

    fixed static points

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ const {
     updateLocationTime,
   } = require("./controller");
   
-app.use("/", express.static(path.join(__dirname, "./client/index.html")));
-app.use(express.static(path.join(__dirname, "./client")));
+app.use("/", express.static(path.join(__dirname, "../client/index.html")));
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.get("/api/locations", getLocations);
 app.delete("/api/locations/:id", deleteLocation);
